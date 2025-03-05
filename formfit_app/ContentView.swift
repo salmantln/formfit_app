2a5fbd350c9b0bd1e6b4b06791c1630599f6f46d
Salman <salma_lart@hotmail.com>
2025-03-05 09:38:27 +0100

    Initial Commit

---
+++ formfit_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  formfit_app
+//
+//  Created by Salman Lartey on 05/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ formfit_app/formfit_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  formfit_appApp.swift
+//  formfit_app
+//
+//  Created by Salman Lartey on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct formfit_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
